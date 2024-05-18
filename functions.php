645664e9a1f82bc918f6fd9c35facff3a22d63bc
Wyllymk <wyllyjeremy@gmail.com>
2024-05-18 18:06:40 +0300

    INitial Changes

---
+++ functions.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Wilson Block Theme functions and definitions
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * @package Wilson Block Theme
+ * @since Wilson Block Theme 1.0
+ */
+
+include get_theme_file_path( 'inc/helpers.php' );
